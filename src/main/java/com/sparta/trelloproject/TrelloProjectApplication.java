5701c509068689fee61ef42b77091ba31701f872
juno0432 <129417004+juno0432@users.noreply.github.com>
2024-10-14 19:07:31 +0900

    git init

---
+++ src/main/java/com/sparta/trelloproject/TrelloProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.trelloproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrelloProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/trelloproject/TrelloProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.trelloproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
