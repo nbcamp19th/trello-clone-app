7113d5708062e0bf67af446f491c73074c5f59a0
ballqs <46590521+ballqs@users.noreply.github.com>
2024-10-16 00:23:41 +0900

    워크스페이스 도메인에 @Vaild 추가

--- src/main/java/com/sparta/trelloproject/domain/workspace/dto/WorkspaceInviteRequestDto.java
+++ src/main/java/com/sparta/trelloproject/domain/workspace/dto/WorkspaceInviteRequestDto.java
@@ -1,10 +1,17 @@
 package com.sparta.trelloproject.domain.workspace.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class WorkspaceInviteRequestDto {
+    @Email
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
     String email;
+    @NotNull
     Long workspaceId;
+    @NotBlank(message = "워크스페이스 역할은 필수 입력 값입니다.")
     String workspaceUserRole;
 }
--- src/main/java/com/sparta/trelloproject/domain/workspace/dto/WorkspaceRequestDto.java
+++ src/main/java/com/sparta/trelloproject/domain/workspace/dto/WorkspaceRequestDto.java
@@ -1,11 +1,13 @@
 package com.sparta.trelloproject.domain.workspace.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class WorkspaceRequestDto {
+    @NotBlank(message = "워크스페이스명은 필수 입력 값입니다.")
     private String workspaceName;
     private String workspaceDescription;
 }
