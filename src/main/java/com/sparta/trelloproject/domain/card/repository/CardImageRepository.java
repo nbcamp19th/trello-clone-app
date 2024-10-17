79286f2ebc9ade78d71b704fcb6f8df23c87ca79
juno0432 <129417004+juno0432@users.noreply.github.com>
2024-10-17 14:07:35 +0900

    feat : 카드 파일 이름 엔티티 추가
    - 카드 이미지 삭제를위해 파일이름 엔티티를 추가하였습니다.

--- src/main/java/com/sparta/trelloproject/domain/card/repository/CardImageRepository.java
+++ src/main/java/com/sparta/trelloproject/domain/card/repository/CardImageRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CardImageRepository extends JpaRepository<CardImage, Long> {
 
     CardImage findByCardId(Long cardId);
-
 }
